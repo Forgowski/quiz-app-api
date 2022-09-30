d237ee29f4457d37892e099e85b54d7fec7861c2
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2022-10-01 01:14:38 +0200

    UI progress

--- quiz_core.py
+++ quiz_core.py
@@ -1,6 +1,6 @@
 import requests
 import time
-import html
+
 
 
 CATEGORIES = ["general knowledge","books","film","music","musicals & theatres","television", "videogames", "board games", "science & nature", "IT", "mythology", "sports", "geography", "politics", "art",
@@ -12,23 +12,34 @@ class QuizCore:
         self.my_category = 11
         self.counter = 0
         self.questionbank = requests.get('https://opentdb.com/api.php', params={"amount" : 10, "category" : self.my_category, "type" : "boolean"}).json()["results"]
-        self.question_list = html.unescape(self.questionbank)
+        self.question_list = self.questionbank
         self.answer = self.question_list[0]["correct_answer"]
         self.question = self.question_list[0]["question"]
         self.is_empty = False
+        self.score = 0
 
 
     def nextQuestion(self):
         if self.counter == 10:
             self.is_empty = True
+
         else:
             self.counter += 1
-            self.answer = self.question_list[counter]["correct_answer"]
-            self.question = self.question_list[counter]["question"]
+            self.answer = self.question_list[self.counter]["correct_answer"]
+            self.question = self.question_list[self.counter]["question"]
 
     def is_end(self) -> bool:
         if self.is_empty:
             return True
 
-Q = QuizCore()
-print(Q.answer)
+    def checkTrue(self):
+        if self.answer == True:
+            self.score += 1
+        self.nextQuestion()
+
+    def checkFalse(self):
+        if self.answer == False:
+            self.score += 1
+        self.nextQuestion()
+
+
--- ui.py
+++ ui.py
@@ -1,19 +1,29 @@
 from quiz_core import QuizCore
 from tkinter import *
+import html
 
 class Ui:
     def __init__(self):
         quiz = QuizCore()
         window = Tk(screenName="Quiz")
         window.config(height=600, width=600, pady=20, padx=20,background="blue")
-        question_place = Canvas(height=300, width=300, background="yellow")
+
+        question_place = Canvas(height=300, width=300, background="#b5e0f0")
         question_place.grid(columnspan=2, row=1, pady=50)
+
         true = PhotoImage(file="pictures/true.png")
-        true_button = Button(image=true, highlightthickness=0)
+        true_button = Button(image=true, highlightthickness=0, command=quiz.checkTrue)
         true_button.grid(column=0, row=3)
+
         false = PhotoImage(file="pictures/false.png")
-        false_button = Button(image=false, highlightthickness=0)
+        false_button = Button(image=false, highlightthickness=0, command=quiz.checkFalse)
         false_button.grid(column=1, row=3)
+
+        score_counter = Label(text=f"score: {quiz.score}",font=("arial", 20), bg="blue")
+        score_counter.grid(row=0, column=1)
+
+        question = Label(text=f"{html.unescape(quiz.question)}",font=("arial", 15), bg="#b5e0f0", wraplength=300)
+        question.grid(columnspan=2, row=1, pady=50)
 
         window.mainloop()
 
